693c086cfab7b2310456c665fc5ac0079fa499bc
gabriel <gabriel.brehault@gmail.com>
2022-11-21 23:17:52 +0100

    Bowls scene

--- src/PendulumApplication.cpp
+++ src/PendulumApplication.cpp
@@ -163,47 +163,158 @@ PendulumApplication::PendulumApplication()
   //}
 
   {
-    // Ground
-    {
-      auto ground = std::make_shared<BoxCollider>(
-        std::make_shared<Box>(glm::vec3(3.0, 20.0, 0.5)));
-      auto groundRB = std::make_shared<RigidBody>(ground, 1e+6, 0.0, true, false);
-      groundRB->translateBy(glm::vec3(0.0, 0.0, -3.0));
-      m_scene->addChild(groundRB);
-    }
 
-    // Ball
-    {
-      auto ball = std::make_shared<SphereCollider>(
-        std::make_shared<Sphere>(1.0));
-      auto ballRB = std::make_shared<RigidBody>(ball, 10.0, 0.2, false, true);
-      ballRB->translateBy(glm::vec3(0.0, -10.0, -2.0));
-      ballRB->addForce(RigidBody::ExternalForce
-                       {
-                         glm::vec3(0.0, 0.0, 0.3), // Position
-                         glm::vec3(0.0, 10.0, 0.0) // Force
-                       });
-      m_scene->addChild(ballRB);
-    }
+    // // --Bowls--
+    // // Ground
+    // {
+    //   auto ground = std::make_shared<BoxCollider>(
+    //     std::make_shared<Box>(glm::vec3(40.0, 10.0, 0.2)));
+    //   auto groundRB = std::make_shared<RigidBody>(ground, 1e+6, 0.0, true, false);
+    //   groundRB->translateBy(glm::vec3(-10.0, 0.0, -3.0));
+    //   m_scene->addChild(groundRB);
+    // }
 
-    // Bowling Pin
-    {
-      auto makePin = [&](glm::vec2 pos)
-      {
-        auto pin = std::make_shared<BoxCollider>(
-          std::make_shared<Box>(glm::vec3(0.5, 0.5, 3)));
-        auto pinRB = std::make_shared<RigidBody>(pin, 5.0, 0.2, false, true);
-        pinRB->translateBy(glm::vec3(pos.x, 5.0 + pos.y, 0.0));
-        m_scene->addChild(pinRB);
-      };
+    // // Jack
+    // {
+    //   auto jack = std::make_shared<SphereCollider>(
+    //     std::make_shared<Sphere>(0.3f));
+    //   auto jackRB = std::make_shared<RigidBody>(jack, 2.0, 0.2, false, true);
+    //   jackRB->translateBy(glm::vec3(-22.0, 0.0, -2.0));
+    //   m_scene->addChild(jackRB);
+    // }
 
-      makePin(glm::vec2(0.0, 0.0));  // 001
-      makePin(glm::vec2(0.5, 1.0));  // 002
-      makePin(glm::vec2(-0.5, 1.0)); // 003
-      makePin(glm::vec2(1.0, 2.0));  // 004
-      makePin(glm::vec2(0.0, 2.0));  // 005
-      makePin(glm::vec2(-1.0, 2.0)); // 006
-    }
+    // // Bowls
+    // {
+    //   auto bowl1 = std::make_shared<SphereCollider>(
+    //     std::make_shared<Sphere>(0.6f));
+    //   auto bowl1RB = std::make_shared<RigidBody>(bowl1, 2.0, 0.2, false, true);
+    //   bowl1RB->translateBy(glm::vec3(-18.0, -1.0, -2.0));
+    //   m_scene->addChild(bowl1RB);
+    // }
+
+    // {
+    //   auto bowl2 = std::make_shared<SphereCollider>(
+    //     std::make_shared<Sphere>(0.6f));
+    //   auto bowl2RB = std::make_shared<RigidBody>(bowl2, 2.0, 0.2, false, true);
+    //   bowl2RB->translateBy(glm::vec3(0.0, 0.0, 2.0));
+    //   bowl2RB->setInitLinearVelocity(glm::vec3(-15.0, 0.0, 2.0));
+    //   m_scene->addChild(bowl2RB);
+    // }
+
+    // // --Pool--
+    // // Ground
+    // {
+    //   auto ground = std::make_shared<BoxCollider>(
+    //     std::make_shared<Box>(glm::vec3(20.0, 10.0, 0.2)));
+    //   auto groundRB = std::make_shared<RigidBody>(ground, 1e+6, 0.0, true, false);
+    //   groundRB->translateBy(glm::vec3(0.0, 0.0, -3.0));
+    //   m_scene->addChild(groundRB);
+    // }
+
+    // // Walls
+    // {
+    //   auto wall1 = std::make_shared<BoxCollider>(
+    //     std::make_shared<Box>(glm::vec3(1.0, 7.0, 1.0)));
+    //   auto wall1RB = std::make_shared<RigidBody>(wall1, 1e+6, 0.0, true, false);
+    //   wall1RB->translateBy(glm::vec3(-10.0, 0.0, -2.5));
+    //   m_scene->addChild(wall1RB);
+    // }    
+
+    // {
+    //   auto wall2 = std::make_shared<BoxCollider>(
+    //     std::make_shared<Box>(glm::vec3(1.0, 7.0, 1.0)));
+    //   auto wall2RB = std::make_shared<RigidBody>(wall2, 1e+6, 0.0, true, false);
+    //   wall2RB->translateBy(glm::vec3(10.0, 0.0, -2.5));
+    //   m_scene->addChild(wall2RB);
+    // }
+
+    // {
+    //   auto wall3 = std::make_shared<BoxCollider>(
+    //     std::make_shared<Box>(glm::vec3(16.5, 1.0, 1.0)));
+    //   auto wall3RB = std::make_shared<RigidBody>(wall3, 1e+6, 0.0, true, false);
+    //   wall3RB->translateBy(glm::vec3(0.0, -5.0, -2.5));
+    //   m_scene->addChild(wall3RB);
+    // }
+
+    // {
+    //   auto wall4 = std::make_shared<BoxCollider>(
+    //     std::make_shared<Box>(glm::vec3(16.5, 1.0, 1.0)));
+    //   auto wall4RB = std::make_shared<RigidBody>(wall4, 1e+6, 0.0, true, false);
+    //   wall4RB->translateBy(glm::vec3(0.0, 5.0, -2.5));
+    //   m_scene->addChild(wall4RB);
+    // }
+
+    // // White Ball
+    // {
+    //   auto whiteBall = std::make_shared<SphereCollider>(
+    //     std::make_shared<Sphere>(0.5f));
+    //   auto whiteBallRB = std::make_shared<RigidBody>(whiteBall, 5.0, 0.2, false, true);
+    //   whiteBallRB->translateBy(glm::vec3(7.0, 0.0, -2.5));
+    //   whiteBallRB->setInitLinearVelocity(glm::vec3(-18.0, 36.0, 0.0));
+    //   m_scene->addChild(whiteBallRB);
+    // }
+
+    // // Balls
+    // {
+    //   auto makeBin = [&](glm::vec2 pos)
+    //   {
+    //     auto ball = std::make_shared<SphereCollider>(
+    //       std::make_shared<Sphere>(0.5));
+    //     auto ballRB = std::make_shared<RigidBody>(ball, 5.0, 0.2, false, true);
+    //     ballRB->translateBy(glm::vec3(-3.0 + pos.x, pos.y, -2.5));
+    //     m_scene->addChild(ballRB);
+    //   };
+
+    //   makeBin(glm::vec2(0.0, 0.0));  // 001
+    //   makeBin(glm::vec2(-1.0, 0.5));  // 002
+    //   makeBin(glm::vec2(-1.0, -0.5)); // 003
+    //   makeBin(glm::vec2(-2.0, 1.0));  // 004
+    //   makeBin(glm::vec2(-2.0, 0.0));  // 005
+    //   makeBin(glm::vec2(-2.0, -1.0)); // 006
+    // }
+
+
+    // // Ground
+    // {
+    //   auto ground = std::make_shared<BoxCollider>(
+    //     std::make_shared<Box>(glm::vec3(3.0, 20.0, 0.5)));
+    //   auto groundRB = std::make_shared<RigidBody>(ground, 1e+6, 0.0, true, false);
+    //   groundRB->translateBy(glm::vec3(0.0, 0.0, -3.0));
+    //   m_scene->addChild(groundRB);
+    // }
+
+    // // Ball
+    // {
+    //   auto ball = std::make_shared<SphereCollider>(
+    //     std::make_shared<Sphere>(1.0));
+    //   auto ballRB = std::make_shared<RigidBody>(ball, 10.0, 0.2, false, true);
+    //   ballRB->translateBy(glm::vec3(0.0, -10.0, -2.0));
+    //   ballRB->addForce(RigidBody::ExternalForce
+    //                    {
+    //                      glm::vec3(0.0, 0.0, 0.3), // Position
+    //                      glm::vec3(0.0, 10.0, 0.0) // Force
+    //                    });
+    //   m_scene->addChild(ballRB);
+    // }
+
+    // // Bowling Pin
+    // {
+    //   auto makePin = [&](glm::vec2 pos)
+    //   {
+    //     auto pin = std::make_shared<BoxCollider>(
+    //       std::make_shared<Box>(glm::vec3(0.5, 0.5, 3)));
+    //     auto pinRB = std::make_shared<RigidBody>(pin, 5.0, 0.2, false, true);
+    //     pinRB->translateBy(glm::vec3(pos.x, 5.0 + pos.y, 0.0));
+    //     m_scene->addChild(pinRB);
+    //   };
+
+    //   makePin(glm::vec2(0.0, 0.0));  // 001
+    //   makePin(glm::vec2(0.5, 1.0));  // 002
+    //   makePin(glm::vec2(-0.5, 1.0)); // 003
+    //   makePin(glm::vec2(1.0, 2.0));  // 004
+    //   makePin(glm::vec2(0.0, 2.0));  // 005
+    //   makePin(glm::vec2(-1.0, 2.0)); // 006
+    // }
 
     // TEST //
     /*{
